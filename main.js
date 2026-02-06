4310aaded5b26a493f9082fa220d70d65aa33ac7
Hinako Yamamura <yhinako0923@gmail.com>
2026-02-06 16:01:52 +0900

    table行数の最大値を追加、ページ読み込み時のfocusメソッドを追加、定数名を短縮化

--- main.js
+++ main.js
@@ -1,56 +1,61 @@
 "use strict";
 
 {
-  document.querySelector('form').addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const zipcode = document.querySelector('input').value;
-    const accessUrl = `https://zipcloud.ibsnet.co.jp/api/search?zipcode=${zipcode}`;
-    const statusEl = document.querySelector('#status');
+  document.querySelector('input').focus();
 
-    statusEl.textContent = '検索中…';
+  document.querySelector("form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const zipcode = document.querySelector("input").value;
+    const accessUrl = `https://zipcloud.ibsnet.co.jp/api/search?zipcode=${zipcode}`;
+    const statusEl = document.querySelector("#status");
+
+    statusEl.textContent = "検索中…";
 
     try {
       const response = await fetch(accessUrl);
       const data = await response.json();
-      
+
       if (data.results === null) {
-        statusEl.textContent = '存在しない郵便番号です';
+        statusEl.textContent = "存在しない郵便番号です";
         return;
       }
 
       console.log(accessUrl);
-      console.log(data);
+      console.log(data.results);
 
-      statusEl.textContent = '';
+      statusEl.textContent = "";
 
       data.results.forEach((item) => {
-        const trElement = document.createElement('tr');
+        const trEl = document.createElement("tr");
 
-        const tdZipElement = document.createElement('td');
-        tdZipElement.textContent = item.zipcode;
+        const tdZipEl = document.createElement("td");
+        tdZipEl.textContent = item.zipcode;
 
-        const tdAddressElement = document.createElement('td');
-        tdAddressElement.textContent = `${item.address1}${item.address2}${item.address3}`;
+        const tdAddressEl = document.createElement("td");
+        tdAddressEl.textContent = `${item.address1}${item.address2}${item.address3}`;
 
-        const tdFuriganaElement = document.createElement('td');
-        tdFuriganaElement.textContent = `${item.kana1}${item.kana2}${item.kana3}`;
+        const tdFuriganaEl = document.createElement("td");
+        tdFuriganaEl.textContent = `${item.kana1}${item.kana2}${item.kana3}`;
 
-        trElement.append(tdZipElement, tdAddressElement, tdFuriganaElement);
+        trEl.append(tdZipEl, tdAddressEl, tdFuriganaEl);
 
-        const tbody = document.querySelector('#addressesTable tbody');
-        tbody.insertBefore(trElement, tbody.firstElementChild);
+        const tbody = document.querySelector("#addressesTable tbody");
+        tbody.insertBefore(trEl, tbody.firstElementChild);
+
+        let i;
+        while (tbody.rows.length > 20) {
+          tbody.removeChild(tbody.lastElementChild);
+        }
 
         e.target.reset();
-
       });
     } catch (err) {
       console.log(err);
-      statusEl.textContent = '通信エラーが発生しました';
+      statusEl.textContent = "通信エラーが発生しました";
     }
   });
 
-  document.querySelector('#resetTbody').addEventListener('click', () => {
-    document.querySelector('tbody').innerHTML = null;
+  document.querySelector("#resetTbody").addEventListener("click", () => {
+    document.querySelector("tbody").innerHTML = null;
   });
-// 画面サイズ変えたときに崩れないよう対応させる
 }
